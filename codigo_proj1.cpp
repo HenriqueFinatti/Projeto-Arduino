c1235b48496133e8643a9540878f2794f9c34d5d
Henrique Finatti <146378917+HenriqueFinatti@users.noreply.github.com>
2024-09-22 14:05:50 -0300

    Add files via upload

---
+++ codigo_proj1.cpp
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal tela(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+  pinMode(8,INPUT_PULLUP);
+  
+  tela.begin(16, 2);
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  home();
+  tela.setCursor(0,0);
+  
+  if(inicia(8)){
+    tela.print("Jogo comeca");
+  }
+}
